2e205c76a664e5137d9f6640ddc5516038f21215
Evan  Markert <evanmarkert@gmail.com>
2019-08-04 02:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+//take an array of names and gift type, loop through the array of names, return another array of strings combining thank you + gift
+function writeCards(array, gift) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push('Thanks you, array[i], for the wonderful ${gift} gift!')
+  }
+  return newArray;
+}
+
+function countdown() {
+  
+}
